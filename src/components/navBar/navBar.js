22f4726672ee609d98dcbd012356b63c0095c5b3
Victor Asiya <vicasasiya@gmail.com>
2025-05-01 20:43:54 +0100

    today is over

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -119,7 +119,21 @@ function NavBar() {
         </svg>
 
         <div className={styles.label_wrap}>
-          <input type="text" />
+          <input type="text" placeholder = 'Search for Products' />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="2"
+            stroke="currentColor"
+            className={styles.search}
+          >
+            <path
+              stroke="round"
+              strokeLinejoin ="round"
+              d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+            />
+          </svg>
         </div>
 
         <div className={styles.container}>
