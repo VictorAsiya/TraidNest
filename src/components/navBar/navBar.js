d090f005ba1c319258a10b57219cd97d63255fbb
Victor Asiya <vicasasiya@gmail.com>
2025-05-01 18:33:17 +0100

    responsiveness done

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -1,31 +1,56 @@
-import React from "react";
-import { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./navBar.module.css";
 import { Link } from "react-router-dom";
 
-function navBar() {
-  // useEffect(() => {
-  //   const runFunctionIfMedium = () => {
-  //     const width = window.innerWidth;
-  //     if (width >= 768 && width < 1024) {
-  //       console.log("✅ Medium screen — run function here");
-  //       // yourFunction(); // Call your actual function here
-  //     }
-  //   };
+function NavBar() {
+  const [showNav, setShowNav] = useState(false);
 
-  //   runFunctionIfMedium(); // Run on first render
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.pageYOffset > 30 && showNav) {
+        setShowNav(false); // Close nav and backdrop on scroll
+      }
+    };
 
-  //   window.addEventListener("resize", runFunctionIfMedium); // Optional: run on resize
-
-  //   return () => {
-  //     window.removeEventListener("resize", runFunctionIfMedium);
-  //   };
-  // }, []);
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [showNav]); // re-run effect when showNav changes
 
   return (
     <header className={styles.header}>
       <div className={styles.first}>
-        <nav className={styles.navBar}>
+        {showNav && (
+          <div
+            onClick={() => setShowNav(false)}
+            style={{
+              height: "100vh",
+              width: "100%",
+              backgroundColor: "rgba(27, 23, 23, 0.6)",
+              position: "absolute",
+              top: "0",
+              zIndex: "8",
+              
+            }}
+          ></div>
+        )}
+        <nav className={`${styles.navBar} ${showNav ? styles.show : ""}`}>
+
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className={styles.close}
+            onClick={() => setShowNav(false)}
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M6 18 18 6M6 6l12 12"
+            />
+          </svg>
+
           <ul className={styles.ul_one}>
             <li>
               <Link to="./" className={styles.line}>
@@ -63,13 +88,15 @@ function navBar() {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke-width="1.5"
+          strokeWidth="1.5"
           stroke="currentColor"
           className={styles.bar}
+          // onClick={()=> setMenuBar(!menubar)}
+          onClick={() => setShowNav(!showNav)}
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            stroke="round"
+            strokeLinejoin="round"
             d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
           />
         </svg>
@@ -80,19 +107,19 @@ function navBar() {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke-width="1.5"
+          strokeWidth="1.5"
           stroke="currentColor"
           className={styles.bar}
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            stroke="round"
+            strokeLinejoin="round"
             d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
           />
         </svg>
-        
+
         <div className={styles.label_wrap}>
-        <input type="text"/>
+          <input type="text" />
         </div>
 
         <div className={styles.container}>
@@ -101,13 +128,13 @@ function navBar() {
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              stroke-width="1.5"
+              strokeWidth="1.5"
               stroke="currentColor"
               className={styles.size}
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                stroke="round"
+                strokeLinejoin="round"
                 d="M20.25 3.75v4.5m0-4.5h-4.5m4.5 0-6 6m3 12c-8.284 0-15-6.716-15-15V4.5A2.25 2.25 0 0 1 4.5 2.25h1.372c.516 0 .966.351 1.091.852l1.106 4.423c.11.44-.054.902-.417 1.173l-1.293.97a1.062 1.062 0 0 0-.38 1.21 12.035 12.035 0 0 0 7.143 7.143c.441.162.928-.004 1.21-.38l.97-1.293a1.125 1.125 0 0 1 1.173-.417l4.423 1.106c.5.125.852.575.852 1.091V19.5a2.25 2.25 0 0 1-2.25 2.25h-2.25Z"
               />
             </svg>
@@ -119,13 +146,13 @@ function navBar() {
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              stroke-width="1.5"
+              strokeWidth="1.5"
               stroke="currentColor"
               className={styles.size}
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                stroke="round"
+                strokeLinejoin="round"
                 d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
               />
             </svg>
@@ -138,4 +165,4 @@ function navBar() {
   );
 }
 
-export default navBar;
+export default NavBar;
--- src/home/index.js
+++ src/home/index.js
@@ -33,13 +33,13 @@ function Home() {
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
-                stroke-width="1.5"
+                strokeWidth="1.5"
                 stroke="currentColor"
                 className={styles.size}
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  stroke="round"
+                  strokeLinejoin="round"
                   d="m19.5 8.25-7.5 7.5-7.5-7.5"
                 />
               </svg>
@@ -50,13 +50,13 @@ function Home() {
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
-                stroke-width="1.5"
+                strokeWidth="1.5"
                 stroke="currentColor"
                 className={styles.size}
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  stroke="round"
+                  strokeLinejoin="round"
                   d="m19.5 8.25-7.5 7.5-7.5-7.5"
                 />
               </svg>
@@ -67,13 +67,13 @@ function Home() {
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
-                stroke-width="1.5"
+                strokeWidth="1.5"
                 stroke="currentColor"
                 className={styles.size}
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  stroke="round"
+                  strokeLinejoin="round"
                   d="m19.5 8.25-7.5 7.5-7.5-7.5"
                 />
               </svg>
@@ -84,13 +84,13 @@ function Home() {
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
-                stroke-width="1.5"
+                strokeWidth="1.5"
                 stroke="currentColor"
                 className={styles.size}
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  stroke="round"
+                  strokeLinejoin="round"
                   d="m19.5 8.25-7.5 7.5-7.5-7.5"
                 />
               </svg>
@@ -111,164 +111,173 @@ function Home() {
       </SC.Section>
 
       <div className={styles.product_div}>
-            <div className={styles.left_div}>
-            <h1>Fairly Used Hisense Fridge</h1>
-            <p className={styles.p}>#50,000</p>
-            <Button title="Buy Now" className={styles.Button} />
+        <div className={styles.left_div}>
+          <h1>Fairly Used Hisense Fridge</h1>
+          <p className={styles.p}>#50,000</p>
+          <Button title="Buy Now" className={styles.Button} />
+        </div>
+
+        <div className={styles.right_div}>
+          <div className={styles.one}>
+            <div className={styles.one_left}>
+              <p>Special Offer</p>
+              <h1>Soft Cushon Chair</h1>
+              <p className={styles.p}>#28,000</p>
+              <Button title="Buy Now" className={styles.Button} />
             </div>
 
-            <div className={styles.right_div}>
-                <div className={styles.one}>
-                    <div className={styles.one_left}>
-                    <p>Special Offer</p>
-                    <h1>Soft Cushon Chair</h1>
-                    <p className={styles.p}>#28,000</p>
-                    <Button title="Buy Now" className={styles.Button} />
-                    </div>
-
-                    <div className={styles.one_right}>
-                    <h3>Hot Sales</h3>
-
-                    <div className={styles.all_products}>
-                        <div className={styles.top}>
-                            <div className={styles.products}>
-                                <img src={one} alt="" />
-                                <p className={styles.product_details}>Washing Machine</p>
-                            </div>
-                            <div className={styles.products}>
-                                <img src={seven} alt="" />
-                                <p className={styles.product_details}>HP Laptop</p>
-                            </div>
-                            <div className={styles.products}>
-                                <img src={three} alt="" />
-                                <p className={styles.product_details}>Air Conditioner</p>
-                            </div>
-                            <div className={styles.products}>
-                                <img src={four} alt="" />
-                                <p className={styles.product_details}>Ceiling Fan</p>
-                            </div>
-                        </div>
-
-                        <div className={styles.bottom}>
-                            <div className={styles.products}>
-                                <img src={five} alt="" />
-                                <p className={styles.product_details}>Hand Saw</p>
-                            </div>
-                            <div className={styles.products}>
-                                <img src={six} alt="" />
-                                <p className={styles.product_details}>Redmi Q5</p>
-                            </div>
-                            <div className={styles.products}>
-                                <img src={two} alt="" />
-                                <p className={styles.product_details}>Microwave</p>
-                            </div>
-                            <div className={styles.products}>
-                                <img src={eight} alt="" />
-                                <p className={styles.product_details}>Behringer Mick</p>
-                            </div>
-                        </div>
-                    </div>
-                    </div>
+            <div className={styles.one_right}>
+              <h3>Hot Sales</h3>
+              <div className={styles.all_products}>
+                <div className={styles.top}>
+                  <div className={styles.products}>
+                    <img src={one} alt="" />
+                    <p className={styles.product_details}>Washing Machine</p>
+                  </div>
+                  <div className={styles.products}>
+                    <img src={seven} alt="" />
+                    <p className={styles.product_details}>HP Laptop</p>
+                  </div>
+                  <div className={styles.products}>
+                    <img src={three} alt="" />
+                    <p className={styles.product_details}>Air Conditioner</p>
+                  </div>
+                  <div className={styles.products}>
+                    <img src={four} alt="" />
+                    <p className={styles.product_details}>Ceiling Fan</p>
+                  </div>
                 </div>
 
-                <div className={styles.two}>
-                    <p
-                    style={{
-                        display: "flex",
-                        justifyContent: "space-between",
-                        alignItems: 'center',
-                        padding: "0 20px",
-                        margin: "0",
-                        height: "5vh",
-                    }}
-                    >
-                    <p>Best Picks for You</p>
-                    <p>Show More</p>
-                    </p>
-                    <div className={styles.listing}>
-                    <div className={styles.each}>
-                    <Button title="Buy Now" className={styles.Button} />
-                        <div>
-                        <img src={nine} alt="" style={{ height: "60px" }} />
-                        </div>
-                        <div>
-                        <p className={styles.product_details}>Handbag</p>
-                        <p style={{ margin: "0", color: "red", fontSize: '12px' }}>#5,000</p>
-                        </div>
-                    </div>
-
-                    <div className={styles.each}>
-                    <Button title="Buy Now" className={styles.Button} />
-                        <div>
-                        <img src={eleven} alt="" style={{ height: "60px" }} />
-                        </div>
-                        <div>
-                        <p className={styles.product_details}>Knife Rack</p>
-                        <p style={{ margin: "0", color: "red", fontSize: '12px' }}>#5,000</p>
-                        </div>
-                    </div>
-
-                    <div className={styles.each}>
-                    <Button title="Buy Now" className={styles.Button} />
-                        <div>
-                        <img src={ten} alt="" style={{ height: "60px" }} />
-                        </div>
-                        <div>
-                        <p className={styles.product_details}>Nike Sneaker</p>
-                        <p style={{ margin: "0", color: "red", fontSize: '12px' }}>#5,000</p>
-                        </div>
-                    </div>
-
-                    <div className={styles.each}>
-                    <Button title="Buy Now" className={styles.Button} />
-                        <div>
-                        <img src={eleven} alt="" style={{ height: "60px" }} />
-                        </div>
-                        <div>
-                        <p className={styles.product_details}>Knife Rack</p>
-                        <p style={{ margin: "0", color: "red", fontSize: '12px' }}>#5,000</p>
-                        </div>
-                    </div>
-
-                    <div className={styles.each}>
-                    <Button title="Buy Now" className={styles.Button} />
-                        <div>
-                        <img src={nine} alt="" style={{ height: "60px" }} />
-                        </div>
-                        <div>
-                        <p className={styles.product_details}>Handbag</p>
-                        <p style={{ margin: "0", color: "red", fontSize: '12px' }}>#5,000</p>
-                        </div>
-                    </div>
-
-                    <div className={styles.each}>
-                    <Button title="Buy Now" className={styles.Button} />
-                        <div>
-                        <img src={twelve} alt="" style={{ height: "50px" }} />
-                        </div>
-                        <div>
-                        <p className={styles.product_details}>LG TV</p>
-                        <p style={{ margin: "0", color: "red", fontSize: '12px' }}>#5,000</p>
-                        </div>
-                    </div>
-
-                    <div className={styles.each}>
-                    <Button title="Buy Now" className={styles.Button} />
-                        <div>
-                        <img src={eleven} alt="" style={{ height: "60px" }} />
-                        </div>
-                        <div>
-                        <p className={styles.product_details}>Knife Rack</p>
-                        <p style={{ margin: "0", color: "red", fontSize: '12px' }}>#5,000</p>
-                        </div>
-                    </div>
-
-                    </div>
-
-                    
-
+                <div className={styles.bottom}>
+                  <div className={styles.products}>
+                    <img src={five} alt="" />
+                    <p className={styles.product_details}>Hand Saw</p>
+                  </div>
+                  <div className={styles.products}>
+                    <img src={six} alt="" />
+                    <p className={styles.product_details}>Redmi Q5</p>
+                  </div>
+                  <div className={styles.products}>
+                    <img src={two} alt="" />
+                    <p className={styles.product_details}>Microwave</p>
+                  </div>
+                  <div className={styles.products}>
+                    <img src={eight} alt="" />
+                    <p className={styles.product_details}>Behringer Mick</p>
+                  </div>
                 </div>
+              </div>
             </div>
+          </div>
+
+          <div className={styles.two}>
+            <span
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                padding: "0 20px",
+                margin: "0",
+                height: "5vh",
+              }}
+            >
+              <p>Best Picks for You</p>
+              <p>Show More</p>
+            </span>
+            <div className={styles.listing}>
+              <div className={styles.each}>
+                <Button title="Buy Now" className={styles.Button} />
+                <div>
+                  <img src={nine} alt="" style={{ height: "60px" }} />
+                </div>
+                <div>
+                  <p className={styles.product_details}>Handbag</p>
+                  <p style={{ margin: "0", color: "red", fontSize: "12px" }}>
+                    #5,000
+                  </p>
+                </div>
+              </div>
+
+              <div className={styles.each}>
+                <Button title="Buy Now" className={styles.Button} />
+                <div>
+                  <img src={eleven} alt="" style={{ height: "60px" }} />
+                </div>
+                <div>
+                  <p className={styles.product_details}>Knife Rack</p>
+                  <p style={{ margin: "0", color: "red", fontSize: "12px" }}>
+                    #5,000
+                  </p>
+                </div>
+              </div>
+
+              <div className={styles.each}>
+                <Button title="Buy Now" className={styles.Button} />
+                <div>
+                  <img src={ten} alt="" style={{ height: "60px" }} />
+                </div>
+                <div>
+                  <p className={styles.product_details}>Nike Sneaker</p>
+                  <p style={{ margin: "0", color: "red", fontSize: "12px" }}>
+                    #5,000
+                  </p>
+                </div>
+              </div>
+
+              <div className={styles.each}>
+                <Button title="Buy Now" className={styles.Button} />
+                <div>
+                  <img src={eleven} alt="" style={{ height: "60px" }} />
+                </div>
+                <div>
+                  <p className={styles.product_details}>Knife Rack</p>
+                  <p style={{ margin: "0", color: "red", fontSize: "12px" }}>
+                    #5,000
+                  </p>
+                </div>
+              </div>
+
+              <div className={styles.each}>
+                <Button title="Buy Now" className={styles.Button} />
+                <div>
+                  <img src={nine} alt="" style={{ height: "60px" }} />
+                </div>
+                <div>
+                  <p className={styles.product_details}>Handbag</p>
+                  <p style={{ margin: "0", color: "red", fontSize: "12px" }}>
+                    #5,000
+                  </p>
+                </div>
+              </div>
+
+              <div className={styles.each}>
+                <Button title="Buy Now" className={styles.Button} />
+                <div>
+                  <img src={twelve} alt="" style={{ height: "50px" }} />
+                </div>
+                <div>
+                  <p className={styles.product_details}>LG TV</p>
+                  <p style={{ margin: "0", color: "red", fontSize: "12px" }}>
+                    #5,000
+                  </p>
+                </div>
+              </div>
+
+              <div className={styles.each}>
+                <Button title="Buy Now" className={styles.Button} />
+                <div>
+                  <img src={eleven} alt="" style={{ height: "60px" }} />
+                </div>
+                <div>
+                  <p className={styles.product_details}>Knife Rack</p>
+                  <p style={{ margin: "0", color: "red", fontSize: "12px" }}>
+                    #5,000
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   );
