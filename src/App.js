9b372f02bab5e90fee759eeb7bae235c410b6c28
Elizabeth Orji <cegscutie4rill@yahoo.com>
2025-07-12 02:43:49 +0100

    added chatbot to react app via app.js

--- src/App.js
+++ src/App.js
@@ -5,14 +5,22 @@ import Footer from "./components/footer/footer";
 import Home from "./home";
 import About from "./about";
 import "./App.css";
+import Chatbot from './components/Chatbot';
 
-function App() {
+ffunction App() {
   return (
+    <>
+      <NavBar />
+      
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/products" element={<h1>Products</h1>} />
       </Routes>
+
+      <Footer />
+      <Chatbot /> {/* 🟢 Always visible, floating on every page */}
+    </>
   );
 }
 
